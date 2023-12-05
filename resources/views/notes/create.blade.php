1c99a21c0614fcb962527185ec4896e4e83d77f1
Piyush Prajapati <piyushprajapati@Piyushs-MacBook-Pro.local>
2023-12-04 20:53:15 -0600

    second commit

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -7,8 +7,6 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
-             
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg mt-6"> 
                          
                         <form action="{{ route('notes.store') }}" method="POST">@csrf
